ce00a638d91565f146faf1e93c61ee37aa093a05
ashes <792634614@qq.com>
2022-10-22 12:48:36 +0800

    update init

--- src/service/initialize.ts
+++ src/service/initialize.ts
@@ -2,8 +2,7 @@ import * as Parse from 'parse';
 import { PARSE_ID, PARSE_KEY, PARSE_MASTER_KET, PARSE_SERVER_URL } from './constant';
 
 const main = () => {
-  const { initialize } = Parse;
-  initialize(PARSE_ID, PARSE_KEY, PARSE_MASTER_KET);
+  Parse.initialize(PARSE_ID, PARSE_KEY, PARSE_MASTER_KET);
   (Parse as any).serverURL = PARSE_SERVER_URL;
 };
 
