32377a2c4187e2edb9172ac186a1e1e703c644d2
ashes <792634614@qq.com>
2022-10-23 19:11:44 +0800

    update parse

--- src/service/initialize.ts
+++ src/service/initialize.ts
@@ -1,8 +1,8 @@
-import * as Parse from 'parse';
+import Parse, { initialize } from 'parse';
 import { PARSE_ID, PARSE_KEY, PARSE_MASTER_KET, PARSE_SERVER_URL } from './constant';
 
 const main = () => {
-  Parse.initialize(PARSE_ID, PARSE_KEY, PARSE_MASTER_KET);
+  initialize(PARSE_ID, PARSE_KEY, PARSE_MASTER_KET);
   (Parse as any).serverURL = PARSE_SERVER_URL;
   Parse.enableLocalDatastore();
   return Parse;
