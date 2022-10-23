fdcea7b841d2d68cc48f40f9826b043ee1cfcc24
ashes <792634614@qq.com>
2022-10-23 22:50:04 +0800

    update login guide

--- src/page/editor/LeftPanel/index.tsx
+++ src/page/editor/LeftPanel/index.tsx
@@ -1,10 +1,10 @@
 import { AddCircle } from '@mui/icons-material';
 import LayersIcon from '@mui/icons-material/Layers';
-import { Tab } from '@mui/material';
+import { Button, Card, CardActions, CardContent, Popover, Tab, Typography } from '@mui/material';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ComponentsLayers from './ComponentsLayers';
 import { LeftPannelContainer } from './style';
 import SkipPreviousOutlinedIcon from '@mui/icons-material/SkipPreviousOutlined';
@@ -20,6 +20,7 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { useHistory, useParams } from 'react-router-dom';
 import { User } from 'parse';
 import { logout } from '@service/userInfo';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
 
 const LeftPanel: React.FC = () => {
   const lang = useLang();
@@ -29,10 +30,9 @@ const LeftPanel: React.FC = () => {
   const panel = useSelector(selectEditorPanel);
   const userRole = useSelector(selectUserRole);
   const history = useHistory();
-
   const isLogin = !!User.current();
-  const isNoUser = activeKey === 'user' && !isLogin;
-
+  const [isShowPopper, setShowPopper] = useState(!isLogin && page !== 'user');
+  const popperRef = useRef<HTMLDivElement>(null);
   const handleChange = (event: React.SyntheticEvent, val: string) => {
     setActiveKey(val);
     if (val !== 'user') {
@@ -76,6 +76,11 @@ const LeftPanel: React.FC = () => {
     }
   }, []); // eslint-disable-line
 
+  const handleCardClose = () => {
+    setShowPopper(false);
+    history.push('/editor/user/auth');
+  };
+
   useEffect(() => {
     const isTurnToCanvas = page === 'user' && userPage !== 'profile' && isLogin;
     isLogin && dispatch(editorAction.setUserRole('User'));
@@ -96,9 +101,9 @@ const LeftPanel: React.FC = () => {
       <TabContext value={activeKey}>
         <div className="dashbroadController">
           <TabList orientation="vertical" onChange={handleChange}>
-            <Tab disabled={isNoUser} label={lang.createComp} value="create" icon={<AddCircle />} />
-            <Tab disabled={isNoUser} label={lang.layerList} value="layer" icon={<LayersIcon />} />
-            <Tab disabled={isNoUser} label={lang.datasourcesList} value="datasource" icon={<StorageIcon />} />
+            <Tab label={lang.createComp} value="create" icon={<AddCircle />} />
+            <Tab label={lang.layerList} value="layer" icon={<LayersIcon />} />
+            <Tab label={lang.datasourcesList} value="datasource" icon={<StorageIcon />} />
             {userRole !== 'Guest' ? (
               <Tab
                 label={lang.user}
@@ -118,9 +123,33 @@ const LeftPanel: React.FC = () => {
             >
               <SkipPreviousOutlinedIcon />
             </div>
-            <div title="log out" className="operations" onClick={handleLogOut}>
-              <LogoutIcon />
+            <div title="log out" className="operations" onClick={handleLogOut} ref={popperRef}>
+              {isLogin ? <LogoutIcon /> : <AccountBoxIcon />}
             </div>
+            <Popover
+              anchorOrigin={{
+                horizontal: 'right',
+                vertical: 'bottom',
+              }}
+              open={isShowPopper}
+              anchorEl={popperRef.current}
+            >
+              <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                  <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    欢迎来到Juggle Data View
+                  </Typography>
+                  <Typography variant="h5" component="div">
+                    成为注册用户获得更好的体验
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button onClick={handleCardClose} size="small">
+                    Learn More
+                  </Button>
+                </CardActions>
+              </Card>
+            </Popover>
           </div>
         </div>
 
