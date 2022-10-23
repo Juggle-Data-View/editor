9e1d09f62d6616f14844d06e69765f5d0c070906
ashes <792634614@qq.com>
2022-10-23 21:40:55 +0800

    update html template

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,8 @@ import { injectCDNScript } from '@components/common/JuggleDVIcon';
 import PageLoading from '@components/common/PageLoading';
 import Providers from '@components/base/Providers';
 import '@assets/style/index.scss';
-import { initializeParse } from '@parse/react';
-import { PARSE_ID, PARSE_KEY, PARSE_SERVER_URL } from '@service/constant';
-
-initializeParse(PARSE_SERVER_URL, PARSE_ID, PARSE_KEY);
-
+import initialServer from '@service/initialize';
+initialServer();
 injectCDNScript();
 
 //Disable MUI error .'Cause MUI debug source map was losed
