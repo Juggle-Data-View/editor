e4d0c918ec9f3d3c531c5be90699b3de6cd05caf
dushihua <shihua.du@compass.com>
2023-02-25 11:20:57 +0800

    add env var check

--- core/src/service/initialize.ts
+++ core/src/service/initialize.ts
@@ -2,9 +2,13 @@ import Parse from 'parse';
 import { PARSE_ID, PARSE_KEY, PARSE_MASTER_KET, PARSE_SERVER_URL } from './constant';
 
 const main = () => {
+  if (!(PARSE_ID && PARSE_KEY && PARSE_MASTER_KET && PARSE_SERVER_URL)) {
+    throw new Error('system env may empty');
+  }
+
+  (Parse as any).serverURL = PARSE_SERVER_URL;
   Parse.initialize(PARSE_ID, PARSE_KEY, PARSE_MASTER_KET);
   Parse.enableLocalDatastore();
-  (Parse as any).serverURL = PARSE_SERVER_URL;
 };
 
 export default main;
